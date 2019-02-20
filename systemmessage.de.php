d79fd6ad08b18117bb06759ab5009716426fbe91
DimaTestLokalise <42958382+DimaTestLokalise@users.noreply.github.com>
2019-02-20 11:30:04 +0200

    Add files via upload

---
+++ systemmessage.de.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'systemmessage.title'                              => 'Wichtiger Hinweis',
+
+    'systemmessage.punished.title'                     => 'Community-Funktionen gesperrt',
+    'systemmessage.punished.text'                      => 'Erfahre warum',
+    'systemmessage.punished.info_action.title'         => 'Community-Richtlinien',
+
+    'systemmessage.picture_rejected.title'             => 'Deine Bilder wurden gelöscht',
+    'systemmessage.picture_rejected.text'              => 'Erfahre warum',
+    'systemmessage.picture_rejected.action.title'      => 'Neues Bild hochladen',
+    'systemmessage.picture_rejected.info_action.title' => 'Unsere Guideline',
+
+    'systemmessage.verify_done.title'                  => 'Verifizierung erfolgreich',
+    'systemmessage.verify_done.text'                   => 'Du bist jetzt ein "verifiziertes Mitglied"',
+
+    'systemmessage.verify_rejected.title'              => 'Verifizierung fehlgeschlagen',
+    'systemmessage.verify_rejected.text'               => 'Erfahre warum',
+    'systemmessage.verify_rejected.action.title'       => 'Jetzt neu verifizieren',
+    'systemmessage.verify_rejected.info_action.title'  => 'Mehr zur Verifizierung',
+
+    'systemmessage_infodialog.punished.text'           => '%text%',
+    'systemmessage_infodialog.verify_rejected.text'    => "Leider konnten wir dich nicht verifizieren. Bitte prüfe Folgendes:\n- Bist du auf deinem Verifizierungsfoto deutlich zu erkennen?\n- Sind dein Usernamen und der Tagescode gut lesbar?\n- Bist du auf deinem Profilbild allein und gut erkennbar abgebildet?",
+    'systemmessage_infodialog.picture_rejected.text'   => 'Deine kürzlich hochgeladenen Bilder entsprechen nicht unserer Bilder-Guideline und wurden entfernt.',
+];
